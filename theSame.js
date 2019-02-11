9cba87c2e9d662a24287264357b9e9f294f3b9b7
omerjamal93 <pomerjamal93@gmail.com>
2019-02-11 18:57:47 +0300

    updade

---
+++ theSame.js
@@ -0,0 +1,11 @@
+function replay(text){
+  if(text.theSame(/[\W_]+/g, "").case()
+  .split("").reverse().input(""){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+
+console.log(replay('nan'));
